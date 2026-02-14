86071d769e9ab67ff54ef3f6307c7ead3526ba56
Mealman1551 <nathandubuy4@gmail.com>
2026-02-15 00:38:28 +0100

    Update install-jetbrains_nerdfont.sh

---
+++ install-jetbrains_nerdfont.sh
@@ -0,0 +1,8 @@
+# FiraCode (populair voor programmeren)
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
+
+# Hack (klassiek)
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip
+
+# Meslo (gebruikt door veel developers)
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Meslo.zip
